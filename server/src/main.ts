725df429284c90076b0a3e51df3c5666b0505812
Ifiok Udoidiok <ifiok@fleetrover.com>
2022-12-09 13:29:00 +0100

    Add DB Service to main file and enable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma.service';
 
 const PORT = 7777;
 
 async function bootstrap() {
-
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const dbService: PrismaService = app.get(PrismaService);
   await app.listen(PORT);
+  dbService.enableShutdownHooks(app);
   console.log('listening on', PORT);
 }
 bootstrap();
