8528df74762c43126e5f113ad2c5ab3ce9436c1e
Ifiok Udoidiok <ifiok@fleetrover.com>
2022-12-09 13:35:18 +0100

    import post Service and Controller to app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostController } from './posts/post.controller';
+import { PostService } from './posts/post.service';
+import { PrismaService } from './prisma.service';
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PostController],
+  providers: [AppService, PostService, PrismaService],
 })
 export class AppModule {}
